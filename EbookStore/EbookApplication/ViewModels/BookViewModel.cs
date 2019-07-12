e1db4ea66494f2c389070b0a78d72cf737d5381d
Administrator <Administrator@CITADEL-9>
2019-07-12 09:53:16 +0530

    Add Create and Edit Forms

--- EbookStore/EbookApplication/ViewModels/BookViewModel.cs
+++ EbookStore/EbookApplication/ViewModels/BookViewModel.cs
@@ -2,11 +2,14 @@
 using System.Collections.Generic;
 using System.Text;
 using EbookDomain.Models;
+using Microsoft.AspNetCore.Http;
 
 namespace EbookApplication.ViewModels
 {
     public class BookViewModel
     {
         public IEnumerable<Book> Books { get; set; }
+        public Book book { get; set; }
+        public IFormFile Image { get; set; }
     }
 }
--- EbookStore/EbookDomain/Interfaces/IBookRepository.cs
+++ EbookStore/EbookDomain/Interfaces/IBookRepository.cs
@@ -8,5 +8,9 @@ namespace EbookDomain.Interfaces
     public interface IBookRepository
     {
         IEnumerable<Book> GetBooks();
+        Book GetBookDetailsById(int bookId);
+        Book Add(Book bookDetails);
+        Book Update(Book bookDetails);
+        Book Delete(int bookId);
     }
 }
