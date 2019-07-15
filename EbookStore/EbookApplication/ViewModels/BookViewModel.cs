25657ea8603e1bbb68e7ac957f3819dfffd8be8c
GUPTHA <GUPTHA@USER-PC>
2019-07-15 16:26:10 +0530

    PDF File Uploads and Create E-Book Screen Chnaged.

--- EbookStore/EbookApplication/ViewModels/BookViewModel.cs
+++ EbookStore/EbookApplication/ViewModels/BookViewModel.cs
@@ -1,6 +1,7 @@
 ﻿using EbookDomain.Models;
 using Microsoft.AspNetCore.Http;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 
 namespace EbookApplication.ViewModels
 {
@@ -10,6 +11,7 @@ namespace EbookApplication.ViewModels
         public IEnumerable<Technology> Technologys { get; set; }
         public Book book { get; set; }
         public Technology technology { get; set; }
+        [Display(Name = "Choose File")]
         public IFormFile Image { get; set; }
         public string ExistingImageUrl { get; set; }
     }
