4ea69a813ead7dffac48a2850524667415ae8afe
Keshav <bhavanamkeshav@gmail.com>
2019-07-24 21:27:26 +0530

    Date validation

--- EbookStore/EbookDomain/Models/Book.cs
+++ EbookStore/EbookDomain/Models/Book.cs
@@ -73,7 +73,7 @@ namespace EbookDomain.Models
         public string Author { get; set; }
 
         [Required, DataType(DataType.Date)]
-        //[RegularExpression(@"^(((0[1-9]|1[012])\/(?!00|29)([012]\d)|(0[13-9]|1[012])\/(29|30)|(0[13578]|1[02])\/31)\/(18|19|20)\d{2}|02\/29\/((18|19|20)(0[48]|[2468][048]|[13579][26])|2000))$", ErrorMessage = "Please enter or select valid date")]
+        [RegularExpression(@"((18|19|20)[0-9]{2}[\-.](0[13578]|1[02])[\-.](0[1-9]|[12][0-9]|3[01]))|(18|19|20)[0-9]{2}[\-.](0[469]|11)[\-.](0[1-9]|[12][0-9]|30)|(18|19|20)[0-9]{2}[\-.](02)[\-.](0[1-9]|1[0-9]|2[0-8])|(((18|19|20)(04|08|[2468][048]|[13579][26]))|2000)[\-.](02)[\-.]29", ErrorMessage = "Please enter or select valid date")]
         //[Range(typeof(DateTime), "1/1/1900", "12/31/9999")]
         [Display(Name = "Published Date")]
         public string PublishedDate { get; set; }
