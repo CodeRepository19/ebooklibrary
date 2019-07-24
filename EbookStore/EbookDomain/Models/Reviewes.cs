6dde3b5ac1dd4cb3f02e29959afbb8e22081a2b7
GUPTHA <GUPTHA@USER-PC>
2019-07-24 18:09:54 +0530

    UI Changess

--- EbookStore/EbookDomain/Models/Reviewes.cs
+++ EbookStore/EbookDomain/Models/Reviewes.cs
@@ -8,8 +8,8 @@ namespace EbookDomain.Models
         public int ReviewId { get; set; }
 
         [Required]
-        [MaxLength(50, ErrorMessage = "ReviewText should be maximum of 50 characters")]
-        [MinLength(4, ErrorMessage = "ReviewText should be minimum of 4 characters")]
+        [MaxLength(50, ErrorMessage = "Review Text should be maximum of 50 characters")]
+        [MinLength(4, ErrorMessage = "Review Text should be minimum of 4 characters")]
         public string ReviewText { get; set; }
 
         [Required]
---
+++ EbookStore/EbookInfraData/Migrations/20190724122328_Bookstable1.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace EbookInfraData.Migrations
+{
+    public partial class Bookstable1 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Description",
+                table: "Book",
+                maxLength: 5000,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldMaxLength: 500);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Description",
+                table: "Book",
+                maxLength: 500,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldMaxLength: 5000);
+        }
+    }
+}
